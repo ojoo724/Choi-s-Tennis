8acbb89a9405ce84b002b468eac6803b81a7562a
ojoo724 <jae03025@naver.com>
2023-11-07 17:39:24 +0900

    11.07 - 레슨, 오시는길 작업

--- index.js
+++ index.js
@@ -19,3 +19,30 @@ window.addEventListener('scroll', function() {
     }
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+  const contactBtn = document.querySelectorAll('.contact-btn');
+  const mapDiv = document.querySelector('.contact-map');
+  const inputDiv = document.querySelector('.contact-input');
+
+  contactBtn.forEach(function(button) {
+    button.addEventListener('click', function(e) {
+      const dataValue = button.dataset.btn;
+      
+      // 기존에 추가된 클래스 제거
+      contactBtn.forEach(function(btn) {
+        btn.classList.remove('btn-active');
+      });
+
+      // 클릭된 버튼에 'active' 클래스 추가
+      button.classList.add('btn-active');
+
+      // 버튼의 dataset 값에 따라 처리
+      mapDiv.classList.toggle('none', dataValue !== 'map');
+      inputDiv.classList.toggle('none', dataValue !== 'input');
+    });
+  });
+
+});
+
+
+// 레슨문의 클릭
